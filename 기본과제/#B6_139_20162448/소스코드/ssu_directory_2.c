1b2e55067c11d29f46bbbb60ffafa0f7cf8888b2
Codejune <kbj9704@gmail.com>
2020-04-12 22:00:24 +0900

    #B6 comment complete

--- 기본과제/#B6_139_20162448/소스코드/ssu_directory_2.c
+++ 기본과제/#B6_139_20162448/소스코드/ssu_directory_2.c
@@ -27,15 +27,15 @@ int ssu_do_grep(void){
 	char *ptr;
 	DIR *dp;
 
-	if(lstat(pathname,&statbuf) < 0){
+	if(lstat(pathname,&statbuf) < 0){ // 파일의 상태정보를 가져옴
 		fprintf(stderr,"lstat error for %s\n",pathname);
 		return 0;
 	}
 
-	if(S_ISDIR(statbuf.st_mode) ==0){//디렉토리가 아닌 경우(일반 파일)
+	if(S_ISDIR(statbuf.st_mode) ==0){ //디렉토리가 아닌 경우(일반 파일)
 		sprintf(command,"%s %s",grep_cmd,pathname);
 		printf("%s : \n",pathname);
-		system(command);//grep 명령어 실행
+		system(command); // grep 명령어로 내용 탐색
 		return 0;
 	}
 
@@ -43,36 +43,36 @@ int ssu_do_grep(void){
 	*ptr++ = '/';
 	*ptr = '\0';//pathname의 마지막에 /를 추가함
 
-	if((dp = opendir(pathname))==NULL) {//pathname에 대한 DIR 구조체 획득
+	if((dp = opendir(pathname))==NULL) { // pathname에 대한 DIR 구조체 획득
 		fprintf(stderr,"opendir error for %s\n",pathname);
 		return 0;
 	}
 
 	while((dirp = readdir(dp)) != NULL)
-		if(strcmp(dirp->d_name,".") && strcmp(dirp->d_name,"..")){//읽은 파일이나 디렉토리가 자신의 디렉토리인 "."이나 부모 디렉토리인 ".."이 아닌 겨우
-			strcpy(ptr,dirp->d_name);//ptr에 파일 이름 저장
+		if(strcmp(dirp->d_name,".") && strcmp(dirp->d_name,"..")){ //읽은 파일이나 디렉토리가 자신의 디렉토리인 "."이나 부모 디렉토리인 ".."이 아닌 겨우
+			strcpy(ptr,dirp->d_name); //ptr에 파일 이름 저장
 
-			if(ssu_do_grep() < 0)
+			if(ssu_do_grep() < 0) // 에러 발생시
 				break;
 		}
-		
-	
+
+
 	ptr[-1] = 0;
 	closedir(dp);
 	return 0;
 }
 
-void ssu_make_grep(int argc, char *argv[]){
+void ssu_make_grep(int argc, char *argv[]) { // grep 명령어 문자열 생성
 	int i;
-	strcpy(grep_cmd,"grep");//grep_cmd에 "grep"복사
+	strcpy(grep_cmd,"grep"); // 명령어 추가
 
 	for(i=1;i<argc-1;i++){
-		strcat(grep_cmd," ");//grep_cmd에 공백 붙임
-		strcat(grep_cmd,argv[i]);//grep_cmd에 argv[i]를 붙임
+		strcat(grep_cmd," "); // 공백 추가
+		strcat(grep_cmd,argv[i]); // 인자 추가
 	}
 }
 
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 	{
 		if(argc<2){
 			fprintf(stderr,"usage: %s <-CVbchilnsvwx> <-num> <-A num> <-B num> <-f file> \n"                               "          <-e> expr <directory>\n",argv[0]);
