ea7eb8f6f73adccb303c2611a57cf55d340e28b6
codejune <kbj9704@gmail.com>
2020-05-17 23:04:32 +0900

    #B16 fix typing error

--- 기본과제/#B16_322_20162448/소스코드/ssu_sigaction_2.c
+++ 기본과제/#B16_322_20162448/소스코드/ssu_sigaction_2.c
@@ -1,14 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
 
-int main(int argc, char *argv[]){
-	if(argc != 2){
-		fprintf(stderr, "usage: %s [Process ID]\n", argv[0]);
+void ssu_check_pending(int signo, char *signame);
+void ssu_signal_handler(int signo);
+
+int main(void)
+{
+	struct sigaction sig_act;
+	sigset_t sig_set;
+
+	sigemptyset(&sig_act.sa_mask); // 시그널 집합 초기화
+	sig_act.sa_flags = 0;
+	sig_act.sa_handler = ssu_signal_handler;
+
+	if (sigaction(SIGUSR1, &sig_act, NULL) != 0) { // SIGUSR1 시그널 처리 등록
+		fprintf(stderr, "sigaction() error\n");
 		exit(1);
 	}
-	else
-		kill(atoi(argv[1]), SIGKILL); // argv[1]에 존재하는 PID에게 SIGKILL 시그널을 보냄
+	else {
+		sigemptyset(&sig_set); // 시그널 집합 초기화
+		sigaddset(&sig_set, SIGUSR1); // 시그널 등록
 
+		if (sigprocmask(SIG_SETMASK, &sig_set, NULL) != 0) { // sig_set procmask..
+			fprintf(stderr, "sigprocmask() error\n");
+			exit(1);
+		}
+		else {
+			printf("SIGUSR1 signals are now blocked\n");
+			kill(getpid(), SIGUSR1); // SIGUSR1 시그널 전달
+			printf("after kill()\n");
+			ssu_check_pending(SIGUSR1, "SIGUSR1"); // SIGUSR1 시그널 유무 확인
+			sigemptyset(&sig_set); // 시그널 집합 초기화
+			sigprocmask(SIG_SETMASK, &sig_set, NULL); // 블록 시그널 설정
+			printf("SIGUSR1 signals are no longer blocked\n");
+			ssu_check_pending(SIGUSR1, "SIGUSR1"); // SIGUSR1 시그널 유무 확인
+		}
+	}
 	exit(0);
+}
+
+void ssu_check_pending(int signo, char *signame) {
+	sigset_t sig_set;
+
+	if (sigpending(&sig_set) != 0)
+		printf("sigpending() error\n");
+	else if (sigismember(&sig_set, signo))
+		printf("a %s signal is pending\n", signame);
+	else
+		printf("%s signals are not pending\n", signame);
+}
+
+void ssu_signal_handler(int signo) {
+	printf("in ssu_signal_handler fucntion\n");
 }
--- 기본과제/#B16_322_20162448/소스코드/ssu_sigsetjmp_1.c
+++ 기본과제/#B16_322_20162448/소스코드/ssu_sigsetjmp_1.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
+#include <setjmp.h>
 
-void ssu_alarm(int signo);
+void ssu_signal_handler(int signo);
 
-int main(void){
+jmp_buf jump_buffer;
 
-	printf("Alarm Setting\n");
-	signal(SIGALRM, ssu_alarm); // SIGALRM에 사용자정의 시그널 등록
-	alarm(2); // 2초후 자기 자신에게 SIGALRM을 보냄
+int main(void)
+{
+	signal(SIGINT, ssu_signal_handler); // 시그널 재정의
 
-	while(1){
-		printf("done\n");
-		pause(); // SIGALRM시그널이 보내지기 전까지 대기
-		alarm(2);
+	while (1) {
+		if (setjmp(jump_buffer) == 0) { // 분기 설정
+			printf("Hit Ctrl-c at anytime ... \n");
+			pause();
+		}
 	}
 
 	exit(0);
 }
 
-void ssu_alarm(int signo){
-	printf("alarm..!!!\n");
+void ssu_signal_handler(int signo) {
+	char character;
+
+	// 시그널 입력 받으면 아래 내용 처리
+	signal(signo, SIG_IGN);
+	printf("Did you hit Ctrl-c?\n" "Do you really want to quit? [y/n] ");
+	character = getchar();
+
+	if (character == 'y' || character == 'Y')
+		exit(0);
+	else {
+		signal(SIGINT, ssu_signal_handler);
+		longjmp(jump_buffer, 1);
+	}
 }
